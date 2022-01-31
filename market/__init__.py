67dfd0f7f6d2b7170fb0f228249709e32a8e905d
rajat <rajatashhpa@gmail.com>
2022-01-31 16:53:28 +0530

    forms

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'c5dade72b01308181f3bcf0d'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = 'info'
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,32 +1,76 @@
 from market import app
-from flask import render_template,redirect,url_for,flash
+from flask import render_template,redirect,url_for,flash,request
 from market.models import Item,User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm
 from market import db
+from flask_login import login_user,logout_user,login_required,current_user
+
 
 @app.route("/")
 @app.route("/home")
 def hello_world():
     return render_template('home.html')
 
-@app.route("/store")
+@app.route("/store",methods = ['GET','POST'])
+@login_required
+
 def hello_world2():
     items = Item.query.all()
-    return render_template('market.html',items = items)
+    purchase_form = PurchaseItemForm()
+    if request.method == 'POST':
+        purchased_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            if current_user.can_purchase(p_item_object):
+                p_item_object.buy(current_user)
+                flash(f"Congratulations you purchased {p_item_object.name} for {p_item_object.price}", category='success')
+            else:
+                flash(f"Unfortunately! you dont have enough money to purchase {p_item_object.name}", category='danger')
+
+        return redirect(url_for('hello_world2'))
+    if request.method == 'GET':
+        items = Item.query.filter_by(owner=None)
+    return render_template('market.html',items = items, purchase_form=purchase_form)
+
+
 @app.route('/register',methods = ['GET','POST'])
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
         user_to_create = User(username =form.username.data,
                               email_address = form.email_address.data,
-                              password_hash = form.password1.data)
+                              password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
+
         return redirect(url_for('hello_world2'))
     if form.errors != {}:
         for err_msg in form.errors.values():
             flash(f'There was an error in creating the user: {err_msg}',category='danger')
 
     return render_template('register.html',form = form )
+@app.route('/login', methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username = form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! you are logged in as:{attempted_user.username}',category='success')
+            return redirect(url_for('hello_world2'))
+        else:
+            flash('Username and Password Dont match, Please Try Again', category='danger')
+    return render_template('login.html', form = form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("hello_world"))
+
 
 
